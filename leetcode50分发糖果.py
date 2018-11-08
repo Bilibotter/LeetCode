8e9a6f066522e7da617b597f6578af5aed2e187e
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-11-08 18:55:16 +0800

    加入快解

--- leetcode50分发糖果.py
+++ leetcode50分发糖果.py
@@ -1,5 +1,15 @@
 class Solution:
     def candy(self, ratings):
+        my = [1]* len(ratings)
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                my[i] = my[i-1]+1
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1] and my[i] <= my[i+1]:
+                my[i] = my[i+1] + 1
+        return sum(my)
+class Solution:
+    def candy(self, ratings):
         """
         :type ratings: List[int]
         :rtype: int
