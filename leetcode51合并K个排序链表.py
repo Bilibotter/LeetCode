7ede6b1daa52dc37140ef9e794a673423d55497a
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-11-08 21:10:31 +0800

    .....

--- leetcode51合并K个排序链表.py
+++ leetcode51合并K个排序链表.py
@@ -1,3 +1,22 @@
+# 学到神解，以后会写得更快更优雅
+# 击败98.87
+class Solution:
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        vector = []
+        for node in lists:
+            while node:
+                vector.append(node)
+                node = node.next
+        head = ListNode(0)
+        cop =head
+        for i in sorted(vector, key=lambda x:x.val):
+            cop.next = i
+            cop = cop.next
+        return head.next
 # 击败86.74%
 class Solution:
     def mergeKLists(self, lists):
