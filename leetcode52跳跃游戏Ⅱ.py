1bf13def3d45a0fddbc6197cd971d90b91db31ee
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-11-09 23:18:56 +0800

    更快

--- leetcode52跳跃游戏Ⅱ.py
+++ leetcode52跳跃游戏Ⅱ.py
@@ -1,16 +1,14 @@
-# 超越90.84%
+# 超越96.4%
 class Solution:
     def jump(self, nums):
         if len(nums) == 1:
             return 0
-        jumps = 1
-        his = 0
+        ans = 1
         l = len(nums)-1
         i = 0
+        now = 0 
         while i+nums[i] < l:
             maxi = 0
-            now = 0
-            k = 0
             num = nums[i]
             for j in range(1, num+1):
                 k = i+j
@@ -18,5 +16,5 @@ class Solution:
                     maxi = nums[k]+j
                     now = k
             i = now
-            jumps += 1
-        return jumps
+            ans += 1
+        return ans
