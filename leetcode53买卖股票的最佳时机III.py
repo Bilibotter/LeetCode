f11c48beaf4def3c21ecb0637b0e7c7204043ce2
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-11-11 19:21:24 +0800

    原来我智商一般啊

---
+++ leetcode53买卖股票的最佳时机III.py
@@ -0,0 +1,34 @@
+# 凡人解
+class Solution:
+    def maxProfit(self, prices):
+        if len(prices) <= 1:
+            return 0
+        n = len(prices)
+        once = [0]
+        twice = [0]
+        mini = prices[0]
+        maxi = prices[-1]
+        for i in range(1, n):
+            p1 = prices[i]
+            p2 = prices[-i]
+            once.append(max(p1-mini, once[-1]))
+            twice.append(max(maxi-p2, twice[-1]))
+            mini = min(mini, p1)
+            maxi = max(maxi, p2)
+        return max([once[i]+twice[-i] for i in range(1, n)])
+        
+# 让我怀疑人生的大神解
+class Spricelutipricen:
+    def maxProfit(self, prices):
+        a1,a2=-float('inf'),-float('inf')
+        b1,b2=0,0
+        for price in prices:
+            # a1为历史最低价的负值
+            if a1<-price:a1=-price
+            # b1为历史最大盈利
+            if b1<price+a1:b1=price+a1
+            # a2 = b1 - 历史最大盈利后的最小值
+            if a2<b1-price: a2=b1-price
+            # b2为总和盈利
+            if b2<a2+price:b2=a2+price
+        return b2
