6ed9abffe9ed5f189085307d39d80d1a17693967
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2018-11-12 13:29:38 +0800

    重新失去人生意义

--- leetcode55插入区间.py
+++ leetcode55插入区间.py
@@ -19,3 +19,15 @@ class Solution:
                     return ans
         ans.append(Interval(start, end))
         return ans
+    
+# 重新失去人生意义，98.6%的大神解
+class Solution:
+    def insert(self, intervals, newInterval):
+        head = newInterval.start
+        tail = newInterval.end
+        left = [interval for interval in intervals if interval.end < head]
+        right = [interval for interval in intervals if interval.start > tail]
+        if len(left) + len(right) < len(intervals):
+            head = min(intervals[len(left)].start, head)
+            tail = max(intervals[-len(right)-1].end, tail)
+        return left + [Interval(head, tail)] + right
